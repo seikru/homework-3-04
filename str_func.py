d64e94197d58f8d6df7f73120b5a5a5958cf372b
seikru <159621102+seikru@users.noreply.github.com>
2024-02-25 12:52:16 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,4 +1,7 @@
 def strUpper(str):
+    """
+    Возвращает строку со всеми заглавными буквами
+    """
     input_text = ("Введите строку: ")
     upper_text = input_text.upper()
     #    print(upper_text)
